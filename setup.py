9d5bf1de63cff725ffb3c18701b34c8a2d28be3a
Sergey Fursov <geyser85@gmail.com>
2021-10-12 23:04:12 +0300

    fix codestyle

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ class TestCommand(Command):
 setup(
     name='django-uidfield',
     version=version,
-    description='django-uidfield is a library which includes class UIDField for models.',
+    description='django-uidfield is a library which includes class '
+                'UIDField for models.',
     long_description=open('README.rst').read(),
     keywords='django model field',
     license='MIT',
